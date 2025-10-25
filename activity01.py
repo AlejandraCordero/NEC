2b15c45c03b3c2d24657af1fb5789c798f7a5de4
María Alejandra Mamani Cordero <alesisima719@gmail.com>
2025-10-25 11:10:33 -0400

    dataset for activity01

---
+++ activity01.py
@@ -0,0 +1,12 @@
+# ACTIVITY 1: Prediction with Supervised Learning Models
+# Maria Alejandra Mamani Cordero
+
+import pandas as pd
+
+data = pd.read_csv('dataset_bolivia.csv')
+
+print(data.head())
+
+# Muestra información general del DataFrame
+print("\nInformación del archivo:")
+print(data.info())
